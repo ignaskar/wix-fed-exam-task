6ad86bdaa3f4a2d6cd583cbfa83d955e48d84ae8
ignaskar <ignas.karpusenkovas@gmail.com>
2020-11-28 15:31:06 +0200

    Part 1a - show content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,6 +32,9 @@ export class App extends React.PureComponent<{}, AppState> {
 		return (<ul className='tickets'>
 			{filteredTickets.map((ticket) => (<li key={ticket.id} className='ticket'>
 				<h5 className='title'>{ticket.title}</h5>
+				<p className="ticket-content">
+					{ticket.content}
+				</p>
 				<footer>
 					<div className='meta-data'>By {ticket.userEmail} | { new Date(ticket.creationTime).toLocaleString()}</div>
 				</footer>
