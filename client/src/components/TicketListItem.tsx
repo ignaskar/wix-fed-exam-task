5f0ae3882bb12110505ec9c5c80f09c543c09161
ignaskar <ignas.karpusenkovas@gmail.com>
2020-11-30 13:40:52 +0200

    Part 1d - bonus task - truncate text

--- client/src/components/TicketListItem.tsx
+++ client/src/components/TicketListItem.tsx
@@ -1,6 +1,7 @@
-import React, {SetStateAction} from 'react';
+import React, {SetStateAction, useState} from 'react';
 import {Ticket} from "../api";
 import LabelList from "./LabelList";
+import Truncate from 'react-truncate';
 
 interface ITicketProps {
     ticket: Ticket;
@@ -10,16 +11,42 @@ interface ITicketProps {
 
 const TicketListItem = ({ticket, idsToHide, setIdsToHide}: ITicketProps) => {
 
+    const [truncated, setTruncated] = useState<boolean>(false);
+    const [expanded, setExpanded] = useState<boolean>(false);
+
+    const handleTruncate = (isTruncated: boolean) => {
+        if (truncated !== isTruncated) {
+            setTruncated(true);
+        }
+    };
+
+    const toggleLines = (e: any) => {
+        e.preventDefault()
+        setExpanded(!expanded);
+    }
+
     const hideTicket = (ticket: Ticket) => {
         setIdsToHide([...idsToHide, ticket.id]);
     }
-
 
     return (
         <li className="ticket">
             <button className="btn btn-hide" onClick={() => hideTicket(ticket)}>hide</button>
             <h5 className='title'>{ticket.title}</h5>
-            <p className="ticket-content">{ticket.content}</p>
+            <p className="ticket-content">
+                <Truncate
+                    lines={!expanded && 3}
+                    ellipsis={(
+                        <span> <button className="btn-show-more" onClick={(e) => toggleLines(e)}>Show More</button></span>
+                    )}
+                    onTruncate={() => handleTruncate(false)}
+                >
+                    {ticket.content}
+                </Truncate>
+                {!truncated && expanded && (
+                    <span> <button className="btn-show-more" onClick={(e) => toggleLines(e)}>Show Less</button></span>
+                )}
+            </p>
             <footer>
                 <div className='meta-data'>By {ticket.userEmail} | { new Date(ticket.creationTime).toLocaleString()}</div>
                 {ticket.labels ? <LabelList labels={ticket.labels} /> : null}
